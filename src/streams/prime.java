597dec32fe35bb8de62f9a84cde23ffcc063b20d
Shoyab <s.manjoor001@gmail.com>
2025-03-30 18:00:59 +0530

    Even Number using Stream

---
+++ src/streams/prime.java
@@ -0,0 +1,16 @@
+package src.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class prime {
+
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(0,5,10,15,20,25,30);
+        List<Integer> even =  nums.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(even);
+    }
+}
