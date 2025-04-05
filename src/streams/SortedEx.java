c69f1168f7e9f9815a3e9555a57516a1d8964ab6
Shoyab <s.manjoor001@gmail.com>
2025-04-05 09:44:03 +0530

    added SortedEx

---
+++ src/streams/SortedEx.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class SortedEx {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> alist = new ArrayList<Integer>();
+        alist.add(45);
+        alist.add(56);
+        alist.add(35);
+        alist.add(20);
+        alist.add(10);
+        alist.add(32);
+        alist.add(68);
+        alist.add(33);
+        System.out.println(alist);
+    }
+}
