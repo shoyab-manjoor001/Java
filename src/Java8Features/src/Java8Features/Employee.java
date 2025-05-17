45a1c6787e41234007dcd352766b90e00e4165b6
Shoyab <s.manjoor001@gmail.com>
2025-05-17 16:30:00 +0530

    Added Example of Functional Interface on User Defined Class

---
+++ src/Java8Features/src/Java8Features/Employee.java
@@ -0,0 +1,34 @@
+package src.Java8Features;
+
+public class Employee {
+
+    private int eno;
+    private String name;
+
+    public Employee(int eno, String name) {
+        this.eno = eno;
+        this.name = name;
+    }
+
+    public int getEno() {
+        return eno;
+    }
+
+    public void setEno(int eno) {
+        this.eno = eno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + eno + ", name: " + name;
+    }
+
+}
---
+++ src/Java8Features/src/Java8Features/Test.java
@@ -0,0 +1,40 @@
+package src.Java8Features;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        ArrayList<Employee> alist = new ArrayList<>();
+        alist.add(new Employee(13, "Aman"));
+        alist.add(new Employee(23, "Sam"));
+        alist.add(new Employee(31, "John"));
+        alist.add(new Employee(65, "Tonny"));
+        alist.add(new Employee(30, "Alfredo"));
+
+        System.out.println("Before Sorting:");
+        for (Employee employee : alist) {
+            System.out.println(employee);
+        }
+
+        // Sorting of Employee on basis of empid (descending order)
+
+        Collections.sort(alist, (e1, e2) -> e1.getEno() < e2.getEno() ? 1 : -1);
+
+        System.out.println("\nAfter Sorting:");
+        for (Employee employee : alist) {
+            System.out.println(employee);
+        }
+
+        // Sorting of Employee on basis of Name (descending order)
+
+        Collections.sort(alist, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+
+        System.out.println("\nAfter Sorting:");
+        for (Employee employee : alist) {
+            System.out.println(employee);
+        }
+    }
+}
