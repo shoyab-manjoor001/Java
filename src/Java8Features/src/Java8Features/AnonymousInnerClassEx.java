3545872b0f3b7ad05c86b10f14b817094a1e6238
Shoyab <s.manjoor001@gmail.com>
2025-05-17 16:53:35 +0530

    Added Annonymous Inner Class and Default msg of Interface Ex

---
+++ src/Java8Features/src/Java8Features/AnonymousInnerClassEx.java
@@ -0,0 +1,24 @@
+package src.Java8Features;
+
+public class AnonymousInnerClassEx {
+
+    public static void main(String[] args) {
+        // Anonymous Inner Class Example..
+
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Inner Thread");
+                }
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Main Thread");
+        }
+    }
+
+}
---
+++ src/Java8Features/src/Java8Features/Test2.java
@@ -0,0 +1,78 @@
+package src.Java8Features;
+
+interface I {
+
+    public void m1();
+
+    public void m2();
+
+    default void say() {
+        System.out.println("Inside Defualt Message");
+    }
+
+}
+
+public class Test2 implements I {
+
+    public static void main(String[] args) {
+
+        // I i = new I() {
+
+        // @Override
+        // public void m1() {
+        // System.out.println("Inside M1 method");
+        // }
+
+        // @Override
+        // public void m2() {
+        // System.out.println("Inside M2 Method");
+        // }
+        // };
+
+        // i.m2();
+        // i.m1();
+
+        Test2 t = new Test2();
+        t.say();
+
+    }
+
+    @Override
+    public void m1() {
+    }
+
+    @Override
+    public void m2() {
+    }
+}
+
+class Test2_1 implements I {
+
+    @Override
+    public void m1() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'm1'");
+    }
+
+    @Override
+    public void m2() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'm2'");
+    }
+}
+
+class Test2_2 implements I {
+
+    @Override
+    public void m1() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'm1'");
+    }
+
+    @Override
+    public void m2() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'm2'");
+    }
+
+}
