a7e3abfc975e14274d02ef45f72c32a07b4ba304
Shoyab <s.manjoor001@gmail.com>
2025-07-06 11:07:23 +0530

    Added TreeSet Example

---
+++ src/CollectionFramework/TreeSetEx.java
@@ -0,0 +1,36 @@
+import java.util.TreeSet;
+
+public class TreeSetEx {
+
+    public static void main(String[] args) {
+
+        TreeSet treeSet = new TreeSet();
+        treeSet.add(12);
+        treeSet.add(52);
+        treeSet.add(74);
+        treeSet.add(55);
+        treeSet.add(120);
+        treeSet.add(145);
+        treeSet.add(19);
+        treeSet.add(172);
+        treeSet.add(172);
+
+        Utility.print.accept(treeSet.size() + "");
+        Utility.print.accept(treeSet + "");
+
+        TreeSet treeSet2 = new TreeSet<>();
+
+        treeSet2.add(new StringBuffer("A"));
+        treeSet2.add(new StringBuffer("B"));
+        treeSet2.add(new StringBuffer("b"));
+        treeSet2.add(new StringBuffer("c"));
+        treeSet2.add(new StringBuffer("D"));
+        treeSet2.add(new StringBuffer("e"));
+        treeSet2.add(new StringBuffer("F"));
+        treeSet2.add(new StringBuffer("g"));
+
+        // Utility.print.accept(treeSet2 + "");
+        System.out.println(treeSet2);
+
+    }
+}
