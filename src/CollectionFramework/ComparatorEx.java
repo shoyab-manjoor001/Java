945389402eaf14ff731a01628e53b9cabfa4ae18
Shoyab <s.manjoor001@gmail.com>
2025-07-06 12:42:38 +0530

    Added Comparator Example

---
+++ src/CollectionFramework/ComparatorEx.java
@@ -0,0 +1,9 @@
+public class ComparatorEx {
+
+    public static void main(String[] args) {
+
+        Utility.print.accept("A".compareTo("Z"));
+        Utility.print.accept("a".compareTo("Z"));
+        Utility.print.accept("a".compareTo("a"));
+    }
+}
