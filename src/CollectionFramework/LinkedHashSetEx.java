d29e9114240da11f518ccd9306e7d54c6f7b9323
Shoyab <s.manjoor001@gmail.com>
2025-07-06 10:43:27 +0530

    added LinkedHashSet Example

---
+++ src/CollectionFramework/LinkedHashSetEx.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+public class LinkedHashSetEx {
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+
+            ll.add((int) (Math.random() * 10));
+        }
+
+        // Utility.print.accept(ll + "");
+
+        LinkedHashSet linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("A");
+        linkedHashSet.add("hashset");
+        linkedHashSet.add(true);
+        linkedHashSet.add(null);
+        linkedHashSet.addAll(ll);
+
+        Utility.print.accept(linkedHashSet.contains("A"));
+        Utility.print.accept(linkedHashSet.getClass().getName());
+        Utility.print.accept(linkedHashSet.toArray().length);
+        Utility.print.accept(linkedHashSet.size());
+
+        Utility.print.accept(linkedHashSet + "");
+    }
+}
