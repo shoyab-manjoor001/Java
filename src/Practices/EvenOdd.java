1eb73fc9f5a576e873f51427e3b8c16f1020d3e0
Shoyab <s.manjoor001@gmail.com>
2025-11-18 14:47:43 +0530

    Added EvenOdd Example

---
+++ src/Practices/EvenOdd.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Map<Boolean, List<Integer>> map = numbers.stream()
+                .collect(Collectors.partitioningBy(i -> i % 2 == 0));
+
+        System.out.println("Even Numbers: " + map.get(true));
+        System.out.println("Odd Numbers: " + map.get(false));
+
+        // Alternatively, printing directly
+        System.out.println("Even Numbers: ");
+        List<Integer> collect = numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+    }
+}
